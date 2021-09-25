0465ceea3946581d83a23cc583d1a30f1dc3b83e
neerajgoswami521 <87235718+neerajgoswami521@users.noreply.github.com>
2021-09-25 21:23:54 +0530

    infiniteScrolling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import './App.css'
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([])
+  const [pageNumber, setPageNumber] = useState(1)
+
+  const fetchPhotos = async(pageNumber) =>{
+    const AccessKey = '5chf1NaB5VKEB_qPBJiaqO3MIz6M-AeMpWh9bhofjL4'
+    const res = await fetch(`https://api.unsplash.com/photos/?client_id=${AccessKey}&page=${pageNumber}&per_page=10`)
+    const data = await res.json();
+    console.log(data)
+    setPhotos(p => [...p, ...data])
+  }
+
+  useEffect(() => {
+   fetchPhotos(pageNumber)
+  }, [pageNumber])
+
+  const loadMore = () =>{
+    setPageNumber(pageNumber + 1)
+  }
+  window.addEventListener('scroll', () => {
+    if (window.scrollY + window.innerHeight >=
+      document.documentElement.scrollHeight) {
+      loadMore();
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Infinite Scrolling</h1>
+      {
+        photos.map((photo , i) => (
+          <div className="photos" key={i}> 
+            <img src={photo.urls.small} alt="pictures" />
+            <p> Bio:{photo.user.bio}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
